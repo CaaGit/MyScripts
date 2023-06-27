14b578e5205efddb67ffb1ecc951737c9f6acc10
Carlos Annes <caannes@caannesit.com>
2023-06-27 10:36:49 +0100

    EncrypgtPassqword

--- Connecting/Connect-ExcCaainfo.ps1
+++ Connecting/Connect-ExcCaainfo.ps1
@@ -1,3 +1,7 @@
-$cred = Get-Credential -UserName administrator@caannesit.info -Message "Type your Credentials"
+#$cred = Get-Credential -UserName "administrator@caannesit.info" -Message "Type your Credentials"
+#$cred | Export-Clixml -Path "F:\PSEncrypt\Info.xml"
+
+
+$cred = Import-Clixml -Path "F:\PSEncrypt\Info.xml"
 $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://10.0.3.3/PowerShell/ -Authentication Basic -Credential $cred
 Import-PSSession $session
